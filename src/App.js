942d0d9cb1d2ccd4d76d30183f8cd82bcfb46196
lakshyajit165 <lakshyajit165@gmail.com>
2018-10-24 12:08:25 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ class App extends Component {
       <Router>
       <Security issuer='https://dev-293945.oktapreview.com/oauth2/default'
                   client_id='0oagukfz4zxbLeyVU0h7'
-                  redirect_uri={'https://lk-project-portfolio.herokuapp.com' + '/implicit/callback'}
+                  redirect_uri={window.location.origin + '/implicit/callback'}
                   onAuthRequired={onAuthRequired} >
             <div className="App"> 
               <Navbar />
               <div className="container">
                   <Route path="/" exact={true} component = {Home}/>
                   <SecureRoute path="/Project" exact={true} component = {Project}/>
-                  <Route path='/login' render={() => <Login baseUrl='https://dev-293945.oktapreview.com' />} />
+                  <Route path='/login' render={() => <Login baseUrl='https://lk-project-portfolio.herokuapp.com/login' />} />
                   <Route path='/implicit/callback' component={ImplicitCallback} />
               </div>
               
